ef37fe7069b4fd6eeb4e5c401672ff326bb20fe5
Jacky Mai <jacky.jy.mai@gmail.com>
2017-08-14 01:03:59 +1200

    Rename execute() method to start() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,20 @@
-import org.gephi.project.api.ProjectController;
-import org.gephi.project.api.Workspace;
 import strategies.CentrePeriphery;
 import strategies.Global;
 import strategies.Local;
 import strategies.MinLeaf;
 
 public class Main {
-    private ProjectController pc;
-    private Workspace workspace;
-
     public static void main(String args[]) {
         Global global = new Global();
-        global.execute();
+        global.start();
 
-        Local local = new Local();
-        local.execute();
-
-        MinLeaf minLeaf = new MinLeaf();
-        minLeaf.execute();
-
-        CentrePeriphery centrePeriphery = new CentrePeriphery();
-        centrePeriphery.execute();
+//        Local local = new Local();
+//        local.start();
+//
+//        MinLeaf minLeaf = new MinLeaf();
+//        minLeaf.start();
+//
+//        CentrePeriphery centrePeriphery = new CentrePeriphery();
+//        centrePeriphery.start();
     }
 }
