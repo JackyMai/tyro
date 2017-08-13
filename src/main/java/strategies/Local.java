8861e2661cd2ad4d49e2411b5b5e6d8ed9dc107e
Jacky Mai <jacky.jy.mai@gmail.com>
2017-08-14 11:35:13 +1200

    Improve local algorithm for completeness

--- src/main/java/strategies/Local.java
+++ src/main/java/strategies/Local.java
@@ -8,18 +8,19 @@ import org.gephi.statistics.plugin.GraphDistance;
 import java.util.Collection;
 
 public class Local extends Strategy {
-    private Collection<Node> uncovered;
+    private Collection<Node> covered;
+
+    public Local(String centralityType) {
+        super(centralityType);
+    }
 
     @Override
     public void execute(Node newcomer) {
-        // Get list of uncovered nodes from input graph
-//        uncovered = graph.getNodes().toCollection();
-
         int radius = (int) distance.getRadius();
 
         // Find node with max centrality from input graph
         Node firstNode = graphModel.factory().newNode();
-        Column column = graphModel.getNodeTable().getColumn(GraphDistance.BETWEENNESS);
+        Column column = graphModel.getNodeTable().getColumn(centralityType);
 
         for (Node n : graph.getNodes().toArray()) {
             Double maxCentrality = (Double) firstNode.getAttribute(column);
@@ -30,20 +31,42 @@ public class Local extends Strategy {
             }
         }
 
+        // Establish an edge between newcomer and node with highest centrality
         Edge edge = graphModel.factory().newEdge(newcomer, firstNode, 0, 1f, false);
         graph.addEdge(edge);
 
+        // Add neighbours within distance d to
         Collection<Node> neighbors = graph.getNeighbors(firstNode, radius - 1).toCollection();
-        uncovered.addAll(neighbors);
+        covered.addAll(neighbors);
 
-//        for (int i = 0; i < iterations && uncovered.size() != 0; i++) {
-//
-//        }
+        for (int i = 0; i < iterations && covered.size() != 0; i++) {
+            Node nextNode = getNextNode(centralityType);
+            edge = graphModel.factory().newEdge(newcomer, nextNode, 0, 1f, false);
+            graph.addEdge(edge);
+
+            // Compute all nodes with distance d from selected node and add it to covered list
+            neighbors = graph.getNeighbors(nextNode, radius - 1).toCollection();
+            covered.addAll(neighbors);
+            covered.remove(nextNode);   // Remove selected node from covered list
+        }
     }
 
     @Override
     public Node getNextNode(String centralityType) {
-        return null;
+        // Find node with max centrality within list of covered node
+        Node nextNode = graphModel.factory().newNode();
+        Column column = graphModel.getNodeTable().getColumn(centralityType);
+
+        for (Node n : covered) {
+            Double maxCentrality = (Double) nextNode.getAttribute(column);
+            Double centrality = (Double) n.getAttribute(column);
+
+            if (centrality >= maxCentrality) {
+                nextNode = n;
+            }
+        }
+
+        return nextNode;
     }
 
 }
