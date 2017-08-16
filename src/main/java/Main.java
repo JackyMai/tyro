6be092a2f55e544e548f7450b54b8cc186fab022
Jacky Mai <jacky.jy.mai@gmail.com>
2017-08-17 01:36:55 +1200

    Add switch statement to make testing more efficient

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,34 @@
+import strategies.CentrePeriphery;
 import strategies.Global;
+import strategies.Local;
+import strategies.MinLeaf;
 
 public class Main {
-    public static void main(String args[]) {
-        Global global = new Global();
-        global.start();
+    private static final String GLOBAL = "global";
+    private static final String LOCAL = "local";
+    private static final String MINLEAF = "minLeaf";
+    private static final String CENTREPERIPHERY = "centrePeriphery";
 
-//        Local local = new Local();
-//        local.start();
-//
-//        MinLeaf minLeaf = new MinLeaf();
-//        minLeaf.start();
-//
-//        CentrePeriphery centrePeriphery = new CentrePeriphery();
-//        centrePeriphery.start();
+    public static void main(String args[]) {
+        String strategy = GLOBAL;
+
+        switch (strategy) {
+            case GLOBAL:
+                Global global = new Global();
+                global.start();
+                break;
+            case LOCAL:
+                Local local = new Local();
+                local.start();
+                break;
+            case MINLEAF:
+                MinLeaf minLeaf = new MinLeaf();
+                minLeaf.start();
+                break;
+            case CENTREPERIPHERY:
+                CentrePeriphery centrePeriphery = new CentrePeriphery();
+                centrePeriphery.start();
+                break;
+        }
     }
 }
