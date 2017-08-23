4345e2b3b8ec31cc9c04d1408cde00d0c2c6e329
Jacky Mai <jacky.jy.mai@gmail.com>
2017-08-24 03:01:32 +1200

    Fix bug where same node gets covered more than once

--- src/main/java/strategies/Local.java
+++ src/main/java/strategies/Local.java
@@ -3,47 +3,56 @@ package strategies;
 import org.gephi.graph.api.Column;
 import org.gephi.graph.api.Edge;
 import org.gephi.graph.api.Node;
-import org.gephi.statistics.plugin.GraphDistance;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public class Local extends Strategy {
-    private Collection<Node> covered;
+    private HashSet<Node> covered;
+    private ArrayList<Node> targets;
 
     @Override
     public void execute(Node newcomer) {
+        covered = new HashSet<>();
+        targets = new ArrayList<>();
+
         int radius = (int) distance.getRadius();
 
-        // Find node with max centrality from input graph
-        Node firstNode = graphModel.factory().newNode();
+        // Find first node with max centrality from input graph
+        Node selectedNode = graphModel.factory().newNode();
         Column column = graphModel.getNodeTable().getColumn(centralityType);
 
         for (Node n : graph.getNodes().toArray()) {
-            Double maxCentrality = (Double) firstNode.getAttribute(column);
+            Double maxCentrality = (Double) selectedNode.getAttribute(column);
             Double centrality = (Double) n.getAttribute(column);
 
             if (centrality >= maxCentrality) {
-                firstNode = n;
+                selectedNode = n;
             }
         }
 
         // Establish an edge between newcomer and node with highest centrality
-        Edge edge = graphModel.factory().newEdge(newcomer, firstNode, 0, 1f, false);
+        Edge edge = graphModel.factory().newEdge(newcomer, selectedNode, 0, 1f, false);
         graph.addEdge(edge);
+        targets.add(selectedNode);
 
-        // Add neighbours within distance d to
-        Collection<Node> neighbors = graph.getNeighbors(firstNode, radius - 1).toCollection();
+        // Add neighbours within distance d to covered list, excluding the initial node
+        Collection<Node> neighbors = graph.getNeighbors(selectedNode).toCollection();
         covered.addAll(neighbors);
 
         for (int i = 0; i < iterations && covered.size() != 0; i++) {
-            Node nextNode = getNextNode(centralityType);
-            edge = graphModel.factory().newEdge(newcomer, nextNode, 0, 1f, false);
+            // Establish edge between newcomer and selected node
+            selectedNode = getNextNode(centralityType);
+            edge = graphModel.factory().newEdge(newcomer, selectedNode, 0, 1f, false);
             graph.addEdge(edge);
+            targets.add(selectedNode);
 
+            // TODO: fix bug where same node may get chosen multiple times
             // Compute all nodes with distance d from selected node and add it to covered list
-            neighbors = graph.getNeighbors(nextNode, radius - 1).toCollection();
+            neighbors = graph.getNeighbors(selectedNode, radius - 1).toCollection();
             covered.addAll(neighbors);
-            covered.remove(nextNode);   // Remove selected node from covered list
+            covered.remove(selectedNode);   // Remove selected node from covered list
         }
     }
 
@@ -58,11 +67,11 @@ public class Local extends Strategy {
             Double centrality = (Double) n.getAttribute(column);
 
             if (centrality >= maxCentrality) {
+//                System.out.println(maxCentrality);
                 nextNode = n;
             }
         }
 
         return nextNode;
     }
-
 }
