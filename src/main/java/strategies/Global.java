26153f61f218623d9ff211760a16066f8a9a10a1
simonct8 <stho244@aucklanduni.ac.nz>
2017-08-27 21:39:38 +1200

    add Visualization Feature
    add Visualization Feature including: JFrame, png, pdf and svg views of
    the graph

--- src/main/java/strategies/Global.java
+++ src/main/java/strategies/Global.java
@@ -2,15 +2,7 @@ package strategies;
 
 import org.gephi.graph.api.Column;
 import org.gephi.graph.api.Edge;
-import org.gephi.graph.api.GraphController;
-import org.gephi.graph.api.GraphModel;
 import org.gephi.graph.api.Node;
-import org.gephi.graph.api.UndirectedGraph;
-import org.gephi.project.api.ProjectController;
-import org.gephi.project.api.Workspace;
-import org.gephi.statistics.plugin.GraphDistance;
-import org.openide.util.Lookup;
-
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -29,14 +21,23 @@ public class Global extends Strategy {
         for (int i = 0; i < iterations && uncovered.size() != 0; i++) {
             // Establish edge between newcomer and selected node
             Node selectedNode = getNextNode(centralityType);
+
+            if(visualise) {
+                selectedNode.setColor(getColor(i));
+                selectedNode.setSize(40);
+            }
+
             targets.add(selectedNode);
             Edge edge = graphModel.factory().newEdge(newcomer, selectedNode, 0, 1f, false);
             graph.addEdge(edge);
 
             // Compute all nodes with distance < rad(G) from selected node and remove from uncovered list
             Collection<Node> neighbors = graph.getNeighbors(selectedNode, radius - 1).toCollection();
-            uncovered.removeAll(neighbors);
+//            uncovered.removeAll(neighbors);
             uncovered.remove(selectedNode);
+            if (visualise) {
+                updateView();
+            }
         }
     }
 
