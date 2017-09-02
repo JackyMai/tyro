2e4b125faa33df977ba0df46771d3a65c43a14f9
Jacky Mai <jacky.jy.mai@gmail.com>
2017-09-03 03:57:10 +1200

    Add Broker-Hybrid to switch statement

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
 import strategies.BrokerExpress;
+import strategies.BrokerHybrid;
 import strategies.CentrePeriphery;
 import strategies.Global;
 import strategies.Local;
-import strategies.MinLeaf;
 import strategies.Community;
 
 public class Main {
     private static final String GLOBAL = "global";
     private static final String LOCAL = "local";
-    private static final String MINLEAF = "minLeaf";
     private static final String CENTREPERIPHERY = "centrePeriphery";
     private static final String BROKEREXPRESS = "brokerExpress";
+    private static final String BROKERHYBRID = "brokerHybrid";
     private static final String COMMUNITY = "community";
 
     public static void main(String args[]) {
@@ -25,10 +25,6 @@ public class Main {
                 Local local = new Local();
                 local.start();
                 break;
-            case MINLEAF:
-                MinLeaf minLeaf = new MinLeaf();
-                minLeaf.start();
-                break;
             case CENTREPERIPHERY:
                 CentrePeriphery centrePeriphery = new CentrePeriphery();
                 centrePeriphery.start();
@@ -36,6 +32,10 @@ public class Main {
             case BROKEREXPRESS:
                 BrokerExpress brokerExpress = new BrokerExpress();
                 brokerExpress.start();
+                break;
+            case BROKERHYBRID:
+                BrokerHybrid brokerHybrid = new BrokerHybrid();
+                brokerHybrid.start();
                 break;
             case COMMUNITY:
                 Community community = new Community();
