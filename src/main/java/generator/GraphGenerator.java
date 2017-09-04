4b603ad308029c2359494095b579948dd054f5fa
Jacky Mai <jacky.jy.mai@gmail.com>
2017-09-04 21:29:26 +1200

    Add method for automated graph generation

--- src/main/java/generator/GraphGenerator.java
+++ src/main/java/generator/GraphGenerator.java
@@ -9,26 +9,49 @@ import org.graphstream.stream.file.FileSink;
 import org.graphstream.stream.file.FileSinkGraphML;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GraphGenerator {
-    private static final int NODE_COUNT = 100;
-
     // Barabasi-Albert Settings
     private static final int MAX_LINKS_PER_STEP = 3;
 
     // Watts-Strogatz Settings
-    private static final int BASE_DEGREE = 2;
-    private static final double REWIRE_PROBABILITY = 0.5;
+    private static final int BASE_DEGREE = 6;    // 4 to 8
+    private static final double REWIRE_PROBABILITY = 0.1;   // Real-world graph lower than 0.1 on avg
 
     // File path for resources
     private static final String FILE_PATH = "src/main/resources/graph/";
+    private static final int INVALID_ID = -1;
+
+    private static final int MAX_NODE_COUNT = 2000;
+    private static final int STEP = 250;
+
+    private static int nodeCount = 250;
 
     public static void main(String[] args) {
-        generateBA();
-        generateWS();
+        generate();
+        // generateBA();
+        // generateWS();
     }
 
-    public static void generateBA() {
+   public static void generate() {
+        // Generate BA graphs
+        while(nodeCount <= MAX_NODE_COUNT) {
+           for(int id=1; id<=20; id++) {
+               generateBA(id);
+               generateWS(id);
+           }
+
+           nodeCount += STEP;
+       }
+   }
+
+    private static void generateBA() {
+        generateBA(INVALID_ID);
+    }
+
+    private static void generateBA(int graphID) {
         Graph graph = new SingleGraph("Barabasi-Albert");
 
         // Between 1 and "n" new links per node added
@@ -37,24 +60,23 @@ public class GraphGenerator {
         gen.addSink(graph);
         gen.begin();
 
-        for(int i=0; i<NODE_COUNT; i++) {
+        for(int i=0; i<nodeCount; i++) {
             gen.nextEvents();
         }
 
         gen.end();
-//        graph.display();
+        // graph.display();
 
-        FileSink fileSink = new FileSinkGraphML();
-        try {
-            fileSink.writeAll(graph, FILE_PATH + "ba_" + NODE_COUNT + ".graphml");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        exportGraph(graph, "barabasi-albert", "ba", graphID);
     }
 
-    public static void generateWS() {
+    private static void generateWS() {
+        generateWS(INVALID_ID);
+    }
+
+    private static void generateWS(int graphID) {
         Graph graph = new SingleGraph("Watts-Strogatz");
-        Generator gen = new WattsStrogatzGenerator(NODE_COUNT, BASE_DEGREE, REWIRE_PROBABILITY);
+        Generator gen = new WattsStrogatzGenerator(nodeCount, BASE_DEGREE, REWIRE_PROBABILITY);
 
         gen.addSink(graph);
         gen.begin();
@@ -64,13 +86,27 @@ public class GraphGenerator {
         }
 
         gen.end();
-//        graph.display(false); // Node position is provided
+        // graph.display(false); // Node position is provided
 
+        exportGraph(graph, "watts-strogatz", "ws", graphID);
+    }
+
+    private static void exportGraph(Graph graph, String folderName, String graphName, int graphID) {
         FileSink fileSink = new FileSinkGraphML();
+        String directory = FILE_PATH + folderName;
         try {
-            fileSink.writeAll(graph, FILE_PATH + "ws_" + NODE_COUNT + ".graphml");
+            Files.createDirectories(Paths.get(directory));  // Create directory is it doesn't already exist
+            String fullPath = directory + "/" + graphName + "_" + nodeCount;
+
+            // Append graph ID to path if it is provided
+            if(graphID >= 0) {
+                fullPath += "_" + String.format("%02d", graphID);
+            }
+
+            fileSink.writeAll(graph, fullPath + ".graphml");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 }
