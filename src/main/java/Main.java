22865b4cd42715eea5047876e7acbfb49d086d44
simonct8 <stho244@aucklanduni.ac.nz>
2017-09-21 20:56:05 +1200

    Remove commented code from Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -48,7 +48,6 @@ public class Main {
 
     public static void main(String args[]) {
         runCompleteTest();
-        // executeStrategy();
     }
 
     private static void executeStrategy(String graphFilePath, String strategy, String testFilePath) {
